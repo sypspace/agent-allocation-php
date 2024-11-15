198381c9570a8ece0b327cefd5b2ffc6e00d6a45
Sukmono Yogi <sukmono.yogi@gmail.com>
2024-11-15 20:34:15 +0700

    Fix minor

--- app/Services/QiscusService.php
+++ app/Services/QiscusService.php
@@ -215,7 +215,7 @@ class QiscusService
                 ->post("/api/v1/admin/service/assign_agent", $form_params);
 
             if ($response->successful()) {
-                return $response->json();
+                return $response->json('data');
             }
 
             return ResponseHandler::error('Failed to fetch data from the API', $response->status(), $response->json('errors'));
