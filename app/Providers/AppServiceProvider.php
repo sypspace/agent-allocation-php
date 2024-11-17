28e59314b1ccc8987e1608484946a830c52029af
Sukmono Yogi <sukmono.yogi@gmail.com>
2024-11-17 23:53:01 +0700

    Ensure dependency injection for Job class was loaded

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use App\Jobs\AssignAgent;
+use App\Jobs\FallbackRoomAssignment;
 use App\Models\RoomQueue;
 use App\Observers\RoomQueueObserver;
+use App\Services\QiscusService;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +30,14 @@ class AppServiceProvider extends ServiceProvider
             return Http::withHeaders([
                 'Qiscus-App-Id' => env('QISCUS_APP_ID')
             ])->baseUrl(env('QISCUS_BASE_URL', 'https://multichannel.qiscus.com'));
+        });
+
+        $this->app->bindMethod([FallbackRoomAssignment::class, 'handle'], function (FallbackRoomAssignment $job, Application $app) {
+            return $job->handle($app->make(QiscusService::class));
+        });
+
+        $this->app->bindMethod([AssignAgent::class, 'handle'], function (AssignAgent $job, Application $app) {
+            return $job->handle($app->make(QiscusService::class));
         });
 
         RoomQueue::observe(RoomQueueObserver::class);
